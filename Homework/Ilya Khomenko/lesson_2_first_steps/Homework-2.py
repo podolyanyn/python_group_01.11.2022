30cb36d367dad06eb211de8044542c181c07a8e5
kali <97478768+Illja1@users.noreply.github.com>
2023-01-11 12:44:37 +0200

    Homework-2

---
+++ Homework/Ilya Khomenko/lesson_2_first_steps/Homework-2.py
@@ -0,0 +1,6 @@
+# Task-2
+print('Ex-1', 'Ex-2', sep='__')
+print('Ex-1', 'Ex-2', end='__\n')
+# Task-3
+print('#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n\n')
+print('#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#')
