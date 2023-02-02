e3e4aa26805a46cf9050331cf124a134ef18aed2
kali <97478768+Illja1@users.noreply.github.com>
2023-02-02 20:17:16 +0200

    Homenko, Task-3

---
+++ Homework/Ilya Khomenko/lesson_3_Variables_and_primitive_data_types/12faa62c-3b4b-4dc9-af55-96ca41c90f9c.py
@@ -0,0 +1,21 @@
+#Task-1
+name = input('Vvedit imya')
+day = input ('Vvedit deny')
+
+print(f'Good day {name}! {day} is a perfect day to learn some python')
+
+#Task-2
+f_name = 'Ilya'
+l_name = 'Homenko'
+
+print('Hello'+ " " + f_name +" "+ l_name +'!')
+
+#Task-3
+a = 12
+b = 3
+print(' a + b = ' + str(a+b))
+print(' a - b = ' + str(a-b))
+print(' a * b = ' + str(a*b))
+print(' a / b = ' + str(a/b))
+print(' a // b = ' + str(a//b))
+print(' a ** b = ' + str(a**b))
